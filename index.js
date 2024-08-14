e6d05394650711ccb3d427d5a77b685a92b853b5
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2024-08-14 11:37:27 -0300

    add if someone enters any room yoo will welcome

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
+const path = require('path');
 const dotenv = require('dotenv')
 
 dotenv.config();
 
 const token = process.env.TOKEN;
+const audioFilePath = path.join(__dirname, 'hello.mp3');
 
 const client = new Client({
     intents: [
@@ -15,6 +18,31 @@ const client = new Client({
 client.once('ready', () => {
     console.log('Bot está online!');
 });
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if (!oldState.channelId && newState.channelId) {
+        const connection = joinVoiceChannel({
+            channelId: newState.channelId,
+            guildId: newState.guild.id,
+            adapterCreator: newState.guild.voiceAdapterCreator,
+        });
+
+        const player = createAudioPlayer();
+        const resource = createAudioResource(audioFilePath);
+
+        player.play(resource);
+
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            connection.subscribe(player);
+        });
+
+        player.on(AudioPlayerStatus.Idle, () => {
+            connection.destroy();
+        });
+    }
+
+});
+
 
 
 client.login(token);
